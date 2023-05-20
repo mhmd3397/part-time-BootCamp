172300a81fb404cdc0326e27746b362924c4229b
mhmd3397 <1142456@student.birzeit.edu>
2023-05-21 01:12:15 +0300

    trying Template Engines

--- Python/flask/flask_fundamentals/hello_flask/hello.py
+++ Python/flask/flask_fundamentals/hello_flask/hello.py
@@ -1,39 +1,12 @@
-# Import Flask to allow us to create our app
 from flask import Flask, render_template
-
-app = Flask(__name__)  # Create a new instance of the Flask class called "app"
+app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    # Instead of returning a string,
-    # we'll return the result of the render_template method, passing in the name of our HTML file
-    return render_template('index.html')
-
-# import statements, maybe some other routes
+def index():
+    # notice the 2 new named arguments!
+    return render_template("index.html", phrase="hello", times=5)
 
 
-@app.route('/success')
-def success():
-    return "success"
-
-# app.run(debug=True) should be the very last statement!
-
-
-# for a route '/hello/____' anything after '/hello/' gets passed as a variable 'name'
-@app.route('/hello/<name>')
-def hello(name):
-    print(name)
-    return "Hello, " + name
-
-
-# for a route '/users/____/____', two parameters in the url get passed as username and id
-@app.route('/users/<username>/<id>')
-def show_user_profile(username, id):
-    print(username)
-    print(id)
-    return "username: " + username + ", id: " + id
-
-
-if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
-    app.run(debug=True)    # Run the app in debug mode.
+if __name__ == "__main__":
+    app.run(debug=True)
