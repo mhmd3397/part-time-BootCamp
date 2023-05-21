b75224755e7d92c99d3be43bbdbdee45258b029f
mhmd3397 <1142456@student.birzeit.edu>
2023-05-22 01:14:48 +0300

    11

--- Python/my_environments/py3Env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\mhmd2\Desktop\AXOS ACADEMY\part-time-BootCamp\Python\my_environments\py3Env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
