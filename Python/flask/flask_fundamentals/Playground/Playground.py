24cd659477ff48a31a050255c2fbbad4bd67c94e
mhmd3397 <1142456@student.birzeit.edu>
2023-05-22 21:46:37 +0300

    some edits

--- Python/flask/flask_fundamentals/Playground/Playground.py
+++ Python/flask/flask_fundamentals/Playground/Playground.py
@@ -6,7 +6,8 @@ Mohammed Eleyan
 
 
 # Internal Styling
-# Just for this assignment, use an internal stylesheet or inline CSS (review here).
+# Just for this assignment,
+#  use an internal stylesheet or inline CSS (review here).
 
 
 from flask import Flask, render_template
@@ -15,8 +16,7 @@ app = Flask(__name__)
 
 # Level 1
 # When a user visits http://localhost:5000/play,
-# # have it render three beautiful looking blue boxes.
-# # Please use a template to render this.
+# have it render three beautiful looking blue boxes.
 
 
 @app.route('/play')
@@ -26,11 +26,7 @@ def index2(times=int(3), color="#9fc5f8"):
 
 # Level 2
 # When a user visits localhost:5000/play/(x),
-# # have it display the beautiful looking blue boxes x times.
-# # For example, localhost:5000/play/7 should display these blue boxes 7 times.
-# # Calling localhost:5000/play/35 would display these blue boxes 35 times.
-# # Please remember that x originally is a string, and if you want to use it as an integer,
-# # you must first convert it to integer using int(). For example int("7") returns 7.
+# have it display the beautiful looking blue boxes x times.
 
 
 @app.route('/play/<times>')
@@ -40,10 +36,7 @@ def index3(times):
 
 # Level 3
 # When a user visits localhost:5000/play/(x)/(color),
-# # have it display beautiful looking boxes x times,
-# # but this time where the boxes appear in (color).
-# # For example, localhost:5000/play/5/green would display 5 beautiful green boxes.
-# # Calling localhost:5000/play/35/red would display 35 beautiful red boxes.
+# have it display beautiful looking boxes x times,
 
 
 @app.route('/play/<times>/<color>')
