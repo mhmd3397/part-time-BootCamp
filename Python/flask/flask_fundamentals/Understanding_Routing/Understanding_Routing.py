4bc146bd3b10bd12842e3189295e2f90973aff88
mhmd3397 <1142456@student.birzeit.edu>
2023-05-26 19:04:11 +0300

    some edits for flake8

--- Python/flask/flask_fundamentals/Understanding_Routing/Understanding_Routing.py
+++ Python/flask/flask_fundamentals/Understanding_Routing/Understanding_Routing.py
@@ -3,7 +3,8 @@ This module is for assignment Understanding Routing in Coding Dojo
 Mohammed Eleyan
 20-05-2023
 '''
-# Create a server file that generates the specified responses for the following url requests:
+# Create a server file that generates the specified responses
+# for the following url requests:
 
 from flask import Flask
 
@@ -35,7 +36,8 @@ def say(name):
     print(name)
     return f"Hi {name}!"
 
-# Create one url pattern and function that can handle the following examples (HINT: int() will come in handy! For example int("35") returns 35):
+# Create one url pattern and function that can handle the following examples
+# (HINT: int() will come in handy! For example int("35") returns 35):
 # localhost:5000/repeat/35/hello - have it say "hello" 35 times
 # localhost:5000/repeat/80/bye - have it say "bye" 80 times
 # localhost:5000/repeat/99/dogs - have it say "dogs" 99 times
