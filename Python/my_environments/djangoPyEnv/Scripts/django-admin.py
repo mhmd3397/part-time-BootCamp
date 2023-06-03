0d50d1856cdc43a26711c7ff123a58a764ce9f3f
mhmd3397 <1142456@student.birzeit.edu>
2023-06-03 22:41:17 +0300

    create django envirinment

---
+++ Python/my_environments/djangoPyEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\mhmd2\Desktop\AXOS ACADEMY\part-time-BootCamp\Python\my_environments\djangoPyEnv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
