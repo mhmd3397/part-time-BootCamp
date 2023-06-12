fe7e21b29afb232c28dc581bce16b005455cd0bb
mhmd3397 <1142456@student.birzeit.edu>
2023-06-13 00:37:08 +0300

    solving Dojo & Ninjas (Shell)

---
+++ Python/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Dojo(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    desc = models.TextField(default="old dojo")
+
+
+class Ninja(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(
+        Dojo, related_name="ninjas", on_delete=models.CASCADE)
