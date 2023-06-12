747a156a93ed6141aad25444271d925560d7c9d4
mhmd3397 <1142456@student.birzeit.edu>
2023-06-12 21:57:53 +0300

    solving Assignment: Users with Templates

--- Python/django/django_orm/single_model_orm/users_app/urls.py
+++ Python/django/django_orm/single_model_orm/users_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.user_list, name='user_list'),
+    path('add_user/', views.add_user, name='add_user'),
 ]
--- Python/django/django_orm/single_model_orm/users_app/views.py
+++ Python/django/django_orm/single_model_orm/users_app/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse('hello')
+def user_list(request):
+    users = User.objects.all()
+    context = {'users': users}
+    return render(request, 'index.html', context)
+
+
+def add_user(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email = request.POST.get('email')
+        age = request.POST.get('age')
+        User.objects.create(first_name=first_name,
+                            last_name=last_name, email_address=email, age=age)
+        return redirect('user_list')
+    return redirect('user_list')
