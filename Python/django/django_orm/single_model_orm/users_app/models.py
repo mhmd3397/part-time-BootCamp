350c5b94d64e3d7106f2a5c42d2bd546f0e3f699
mhmd3397 <1142456@student.birzeit.edu>
2023-06-12 19:00:35 +0300

    solve Assignment: Users (Shell)

---
+++ Python/django/django_orm/single_model_orm/users_app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email_address = models.CharField(max_length=255)
+    age = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
