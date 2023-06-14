cbc8c7cf0d361719f9476e0c500d1aa4a9c3f110
mhmd3397 <1142456@student.birzeit.edu>
2023-06-14 20:10:15 +0300

    solve Assignment: Dojos & Ninjas with Template Objectives:

--- Python/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/urls.py
+++ Python/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.dojos, name='dojos'),
+    path('create-dojo/', views.create_dojo, name='create-dojo'),
+    path('create-ninja/', views.create_ninja, name='create-ninja'),
+    path('delete-dojo/<int:dojo_id>/', views.delete_dojo, name='delete-dojo'),
+    path('delete-ninja/<int:ninja_id>/',
+         views.delete_ninja, name='delete-ninja'),
 ]
--- Python/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ Python/django/django_orm/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,7 +1,43 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect
+from .models import Dojo, Ninja
 
-# Create your views here.
+
+def dojos(request):
+    dojos = Dojo.objects.all()
+    return render(request, 'index.html', {'dojos': dojos})
 
 
-def index(request):
-    HttpResponse("hello")
+def create_dojo(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        city = request.POST.get('city')
+        state = request.POST.get('state')
+        desc = request.POST.get('desc')
+        Dojo.objects.create(name=name, city=city, state=state, desc=desc)
+    return redirect('dojos')
+
+
+def create_ninja(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        dojo_id = request.POST.get('dojo')
+        dojo = Dojo.objects.get(id=dojo_id)
+        Ninja.objects.create(first_name=first_name,
+                             last_name=last_name, dojo=dojo)
+    return redirect('dojos')
+
+
+def delete_dojo(request, dojo_id):
+    if request.method == 'POST':
+        dojo = Dojo.objects.get(id=dojo_id)
+        dojo.ninjas.all().delete()  # Delete all associated ninjas
+        dojo.delete()  # Delete the dojo
+    return redirect('dojos')
+
+
+def delete_ninja(request, ninja_id):
+    if request.method == 'POST':
+        ninja = Ninja.objects.get(id=ninja_id)
+        ninja.delete()  # Delete the ninja
+    return redirect('dojos')
