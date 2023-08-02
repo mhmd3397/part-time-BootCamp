d72ae12f9db802f9421e069a24d1b23884d8167e
mhmd3397 <1142456@student.birzeit.edu>
2023-08-03 01:13:49 +0300

    Create Contains.js

---
+++ ALGO/Linked_Lists/Contains.js
@@ -0,0 +1,22 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+    contains(value) {
+        let runner = this.head;
+        while (runner) {
+            if (runner.data === value) {
+                return true;
+            }
+            runner = runner.next;
+        }
+        return false;
+    }
+}
