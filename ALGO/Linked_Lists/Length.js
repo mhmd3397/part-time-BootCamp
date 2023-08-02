8b20046bc5b8225975290124fe920206ba24d4f4
mhmd3397 <1142456@student.birzeit.edu>
2023-08-03 01:16:40 +0300

    Create Length.js

---
+++ ALGO/Linked_Lists/Length.js
@@ -0,0 +1,22 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class SLL {
+    constructor() {
+        this.head = null;
+    }
+
+    length() {
+        let count = 0;
+        let current = this.head;
+        while (current) {
+            count++;
+            current = current.next;
+        }
+        return count;
+    }
+}
